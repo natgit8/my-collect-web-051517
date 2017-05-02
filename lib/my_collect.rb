c7e6433bcb0b94949f0b4cea7ed646b880983e02
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-02 16:37:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+
+def my_collect(array)
+  new_arr = []
+
+  i=0
+  while i < array.length
+    new_arr.push yield(array[i])
+    i += 1
+  end
+  new_arr
+end
+
+my_collect(students) do |name|
+  name.split(" ").first
+end
 
 
+my_collect(collection) do |lang|
+  lang.upcase
+end
